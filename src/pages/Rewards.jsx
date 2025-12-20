58ee0a2eb24dca6ff92ad4719115873fdc62883b
Indy frns <indyfrancois6@gmail.com>
2025-12-20 15:21:17 +0000

    File changes

--- src/pages/Rewards.jsx
+++ src/pages/Rewards.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '../utils';
-import { useQuery, useMutation, useQueryClient } from '@tantml:react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { getCurrentWeekContract, createWeeklyContract, commitToContract, calculateWeeklyHabitScore } from '../functions/businessLogic';
 import { base44 } from '@/api/base44Client';
 import { format, startOfWeek, addDays } from 'date-fns';
