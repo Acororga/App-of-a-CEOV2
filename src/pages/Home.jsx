607e26287dc1fa77ec6134bcfc7d053aa95da78f
Indy frns <indyfrancois6@gmail.com>
2025-12-25 15:14:29 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -182,7 +182,7 @@ export default function Home() {
 
 
   return (
-    <div className="min-h-screen bg-black text-white p-6 pt-20 relative">
+    <div className="min-h-screen bg-black text-white p-6 pt-20 relative overflow-hidden">
       {/* Noise texture */}
       <div className="fixed inset-0 pointer-events-none opacity-[0.015]" style={{
         backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='2.5' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
